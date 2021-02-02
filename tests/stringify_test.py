c38406a54054d48a110fcf9a769960936d75eea0
Kavi Gupta <kavi@berkeley.edu>
2021-02-02 00:09:56 -0800

    regression tests for numpy and torch stringification

--- tests/stringify_test.py
+++ tests/stringify_test.py
@@ -1,7 +1,10 @@
 import unittest
 import json
 
-from permacache import stringify
+import numpy as np
+import torch
+
+from permacache import stringify, stable_hash
 
 
 class StringifyTest(unittest.TestCase):
@@ -19,4 +22,16 @@ class StringifyTest(unittest.TestCase):
 
         self.assertEqual(
             json.dumps({".attr.__name__": "X", "y": "hello"}), stringify(X("hello"))
+        )
+
+    def test_stringify_numpy(self):
+        self.assertEqual(
+            "16469cff96525e3e190758d793e61f9d798cb87617787bc1312cf7a8b59aa4b2",
+            stable_hash(np.random.RandomState(0).randn(1000)),
+        )
+
+    def test_stringify_torch(self):
+        self.assertEqual(
+            "16469cff96525e3e190758d793e61f9d798cb87617787bc1312cf7a8b59aa4b2",
+            stable_hash(torch.tensor(np.random.RandomState(0).randn(1000))),
         )
