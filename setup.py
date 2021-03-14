c9d4b5eb95077d1f6f9732b389ed885c52197243
Kavi Gupta <kavi@berkeley.edu>
2021-03-13 22:31:09 -0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="permacache",
-    version="1.3.0",
+    version="2.0.0",
     author="Kavi Gupta",
     author_email="permacache@kavigupta.org",
     description="Permanant cache.",
