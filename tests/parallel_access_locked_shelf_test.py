6d188a1774a7d9a2512617ad69afd4e95191bb2c
Kavi Gupta <kavi@berkeley.edu>
2022-03-07 18:29:41 -0500

    formatting

--- tests/parallel_access_locked_shelf_test.py
+++ tests/parallel_access_locked_shelf_test.py
@@ -9,21 +9,25 @@ from permacache.locked_shelf import LockedShelf
 
 
 class LockedShelfTest(unittest.TestCase):
-
     def get_then_set_test(self):
         for _ in range(100):
             shelf1 = LockedShelf("temp/tempshelf", multiprocess_safe=True)
             shelf2 = LockedShelf("temp/tempshelf", multiprocess_safe=True)
+
             def set1():
                 with shelf1 as f:
                     f["a"] = 2
+
             def set2():
                 with shelf2 as f:
                     f["b"] = 3
+
             def close1():
                 shelf1.close()
+
             def close2():
                 shelf2.close()
+
             queues = [[set1, close1], [set2, close2]]
             while any(queues):
                 idx = np.random.choice(len(queues))
