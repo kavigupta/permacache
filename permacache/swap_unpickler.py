08a1eb4cec9c4347f486e30b62584135eff4f78a
Kavi Gupta <kavi@berkeley.edu>
2024-10-15 02:48:45 -0400

    Built in pickle remapper (#6)

--- permacache/swap_unpickler.py
+++ permacache/swap_unpickler.py
@@ -1,5 +1,6 @@
 import pickle
 import shelve
+from typing import Dict, Tuple, Union
 
 
 class swap_unpickler_context_manager:
@@ -18,3 +19,39 @@ class swap_unpickler_context_manager:
             self._previous_unpickler = None
         else:
             shelve.Unpickler = pickle.Unpickler
+
+
+def renamed_symbol_unpickler(
+    symbol_rename_map: Dict[Tuple[str, str], Union[Tuple[str, str], type]]
+) -> type:
+    """
+    Returns an unpickler class that renames symbols as specified in
+    the symbol_rename_map dictionaries.
+
+    :param symbol_rename_map: A dictionary mapping (module, name) pairs to
+        (new_module, new_name) pairs. Can also map to a type, in which case
+        we convert the type to a (module, name) pair.
+    """
+
+    symbol_rename_map_string = {}
+    for (module, name), new_symbol in symbol_rename_map.items():
+        if isinstance(new_symbol, type):
+            new_symbol = (new_symbol.__module__, new_symbol.__name__)
+        assert (
+            isinstance(new_symbol, tuple)
+            and len(new_symbol) == 2
+            and all(isinstance(x, str) for x in new_symbol)
+        ), f"Invalid new symbol: {new_symbol}"
+        symbol_rename_map_string[(module, name)] = new_symbol
+
+    class RenamedSymbolUnpickler(pickle.Unpickler):
+        def find_class(self, module, name):
+            if (module, name) in symbol_rename_map_string:
+                module, name = symbol_rename_map_string[(module, name)]
+            try:
+                return super().find_class(module, name)
+            except:
+                print("Could not find", (module, name))
+                raise
+
+    return RenamedSymbolUnpickler
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="permacache",
-    version="3.7.3",
+    version="3.8.0",
     author="Kavi Gupta",
     author_email="permacache@kavigupta.org",
     description="Permanant cache.",
